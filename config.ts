bf6cc3186dc868b61de58af3d2853daaf8a62d51
wonbae <judiking1@naver.com>
2025-11-23 23:20:53 +0900

    config 파일 .env.local로 전환

--- config.ts
+++ config.ts
@@ -1,25 +1,12 @@
 export const clientConfig = {
-  projectId:
-    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? 'reboot-badminton',
-  apiKey:
-    process.env.NEXT_PUBLIC_FIREBASE_API_KEY ??
-    'AIzaSyC-RV0DJURYoXuLGjgmD0Q8JRw-xGOW6P0',
-  authDomain:
-    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ??
-    'reboot-badminton.firebaseapp.com',
-  databaseURL:
-    process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL ??
-    'reboot-badminton.firebaseio.com',
-  messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? '922071552287',
-  storageBucket:
-    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ??
-    'reboot-badminton.appspot.com',
-  appId:
-    process.env.NEXT_PUBLIC_FIREBASE_APP_ID ??
-    '1:922071552287:web:4f0f4747c31da46f60202a',
-  measurementId:
-    process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID ?? 'G-K4T7BQQ6BF',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const NAVER_API_KEY = 'b665agx8f3';
